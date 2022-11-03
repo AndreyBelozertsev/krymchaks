bb08d62f3738d7b1c0dc97ce6ebfca1ff33a3812
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2022-11-03 19:36:38 +0300

    HomePageController setUp

---
+++ app/Http/Controllers/HomePageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Place;
+use App\Models\AboutType;
+use Illuminate\Http\Request;
+
+class HomePageController extends Controller
+{
+
+    public function __invoke()
+    {
+        $mainPost = Post::active()->select(['id','title','slug','description','created_at'])->isFixed()->first();
+
+        $posts = Post::active()->select(['id','title','slug','thumbnail','description','created_at'])->latest()->where('id', '!=', $mainPost ? $mainPost->id : null)->limit(5)->get();
+
+        $types = AboutType::active()->isFixed()->latest()->limit(6)->get();
+
+        $places = Place::active()->get();
+
+        return view('page.home',[
+                                    'mainPost'=>$mainPost,
+                                    'posts'=>$posts,
+                                    'types'=>$types,
+                                    'places'=> $places
+                                ]);
+    }
+}
